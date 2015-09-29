39019e673aa40b1d0264e6dad6465b071706a260
datagutten <datagutten@datagutten.net>
2015-09-29 08:30:08 +0200

    Rewrite with DOM HTML generation

--- renamevideo.php
+++ renamevideo.php
@@ -3,10 +3,10 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Rename video</title>
+<link href="renamevideo.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <?Php
-$starttime=time();
 ini_set('display_errors',1);
 error_reporting('E_ALL');
 
@@ -43,31 +43,35 @@ if(file_exists('tvdb_mappings.php'))
 require 'eitparser.php';
 $eitparser=new eitparser;
 
-if (isset($_POST['button']))
+require 'tools/DOMDocument_createElement_simple.php';
+$dom=new DOMDocumentCustom;
+$dom->formatOutput = true;
+
+if(isset($_POST['button']))
 {
-	for ($i=0; $i<count($_POST['navn']); $i++)
+	for ($i=0; $i<count($_POST['name']); $i++)
 	{
-		$newname=$_POST['navn'][$i];
+		$newname=$_POST['name'][$i];
 		if ($newname!='')
 		{
 			$oldname=$_POST['basename'][$i];
 			$newname=str_replace(array(':','?','/'),array(' - ','',''),$newname);
 			
 			$oldfilename=$dir_video.'/'.$oldname;
-			if ($folder=='NRK' || strpos($folder,'input')!==false || strpos($folder,'diverse')!==false)
-				$newfilename=$dir_video.'/'.$newname; //Ikke bruk navnet på samlemapper i filnavn
-			elseif (strpos($oldname,'HD'))
-				$newfilename=$dir_video.'/'.$folder.' '.$newname.' HD'; //Er det tatt opp fra en HD kanal, legg til HD i filnavn
+			if(isset($config['no_name_folders']) && array_search($folder,$config['no_name_folders'])!==false)
+				$newfilename=$dir_video.'/'.$newname; //Do not add folder name to files in folders specified in config
 			else
-				$newfilename=$dir_video.'/'.$folder.' '.$newname; //Lag filnavn med seriens navn
-			
+				$newfilename=$dir_video.'/'.$folder.' '.$newname; //Add the folder name to the file
+			if(strpos($oldname,'HD'))
+				$newfilename.=' HD'; //If recorded from an HD channel, add HD to the file name
+
 			if(file_exists($newfilename.'.ts')) //Handle existing files
 			{
 				$newfilename.="_dupe_".time();
 				echo "Dupe: $newname<br />\n";
 			}
-				
-			if ($newname!='del' && !file_exists($newfilename.'.xml')) //Ikke lag nfo for filer som skal slettes
+
+			if ($newname!='del' && !file_exists($newfilename.'.xml')) //Do not write info file for files to be deleted
 			{
 				$xmlprogram=$guide->recordinginfo($oldname.'.ts'); //Write xmltv data to file
 				if(is_object($xmlprogram))
@@ -75,13 +79,12 @@ if (isset($_POST['button']))
 			}
 			if(!file_exists($dir_delete)) //Create folder for removed files
 				mkdir($dir_delete);
-
 			
 			if(file_exists($dir_snapshots=$config['snapshotpath'].'/'.$_POST['basename'][$i].'.ts')) //Check if snapshot folder exists
 			{
 				if($newname=='del')
 				{
-					if(!file_exists($config['snapshotpath'].'/delete'))
+					if(!file_exists($config['snapshotpath'].'/delete')) //Create folder for removed snapshots
 						mkdir($config['snapshotpath'].'/delete');
 					rename($dir_snapshots,$config['snapshotpath'].'/delete/'.$_POST['basename'][$i]);
 				}
@@ -93,10 +96,10 @@ if (isset($_POST['button']))
 				
 			foreach ($extensions as $extension)
 			{
+				//Add extension to the file name
 				$oldfile=$oldfilename.$extension;
-				$newfile=$newfilename.$extension; //Lag filnavn med type
-				//echo $oldname.$extension.'>'.$newfile.":<br>";		
-				//var_dump($oldfile);
+				$newfile=$newfilename.$extension;
+
 				if(file_exists($oldfile))
 				{
 					if($newname=='del')
@@ -126,12 +129,10 @@ $dir=scandir($dir_video);
 $dir=array_diff($dir,array('.','..','Thumbs.db'));
 
 sort($dir);
-$i=0; 
 
-?>
-<form id="form1" name="form1" method="post" action="">
-<table border=1>
-<?Php
+$form=$dom->createElement_simple('form',false,array('method'=>'post'));
+$table=$dom->createElement_simple('table',$form,array('border'=>'1'));
+
 $count=0;
 foreach ($dir as $key=>$file)
 {
@@ -142,6 +143,10 @@ foreach ($dir as $key=>$file)
 		break;
 	$count++;
 
+	$table->appendChild($tr=$dom->createElement('tr')); //Row for recording
+	
+	$td_file=$dom->createElement_simple('td',$tr,array('class'=>'filename'),$file);
+	$td_description=$dom->createElement_simple('td',$tr,array('class'=>'description'));
 	$displaytext='';
 
 	if(($xmlprogram=$guide->recordinginfo($file))!==false) //Get info from XML
@@ -153,41 +158,63 @@ foreach ($dir as $key=>$file)
 			var_dump($guide->error);
 		}
 		$starttimestamp=strtotime($xmlprogram->attributes()->start);
-	
+
 		if(isset($starttimestamp))
-			$displaytext.=date('H:i',$starttimestamp)."&nbsp;";
+			$dom->createElement_simple('span',$td_description,array('class'=>'starttime'),date('H:i',$starttimestamp)); //Add a span with the start time to the td
 		if(isset($xmlprogram->title)) //Get the title
 		{
-			$displaytext.=' '.$xmlprogram->title;
+			$dom->createElement_simple('span',$td_description,array('class'=>'title'),(string)$xmlprogram->title);
 			$programinfo['xml']['title']=(string)$xmlprogram->title;
 		}
 		if(isset($xmlprogram->category)) //Get the category
 		{
 			if(!is_array($xmlprogram->category))
-				$displaytext.=' - '.$xmlprogram->category."<br />\n";
+				$category=' - '.$xmlprogram->category;
 			else
-				$displaytext.=' - '.$xmlprogram->category[1]."<br />\n";
+			{
+				$category=' - '.$xmlprogram->category[1];
+				$category.=print_r($xmlprogram->category,true);
+			}
+			
+			$span_category=$dom->createElement('span',$category);
+			$span_category->setAttribute('class','category');
+			$td_description->appendChild($span_category);
+		}
+		if(isset($xmlprogram->{'sub-title'})) //Get the sub-title
+		{
+			$dom->createElement_simple('p',$td_description,array('class'=>'title'),(string)$xmlprogram->{'sub-title'});
+			$programinfo['xml']['sub-title']=(string)$xmlprogram->{'sub-title'};
 		}
 		if(isset($xmlprogram->desc)) //Get the description
 		{
-			$displaytext.=$xmlprogram->desc."<br />\n";
+			$td_description->appendChild($dom->createElement('br'));
+			$span_description=$dom->createElement('span',(string)$xmlprogram->desc);
+			$td_description->appendChild($span_description);
+			
 			$programinfo['xml']['description']=(string)$xmlprogram->desc;
 		}
 		if(isset($xmlprogram->{'episode-num'}) && $episodestring=$guide->seasonepisode($xmlprogram)) //Get the episode-num string and convert it to season and episode
 		{
 			$programinfo['xml']['seasonepisode']=$guide->seasonepisode($xmlprogram,false);
-			$displaytext.=$episodestring."<br />\n";
+			$td_description->appendChild($dom->createElement('br'));
+			$span_description=$dom->createElement_simple('span',$td_description,array('class'=>'seasonepisode'),$episodestring);
 		}
+		$recording_start=strtotime($info['datetime']);
+		$offset=$starttimestamp-$recording_start;
+		if($offset<0)
+			$td_description->setAttribute('class','category error');
+		elseif($offset<60*5 || $offset>60*10)
+			$td_description->setAttribute('class','category warning');
 	}
-	elseif(empty($guide->error))
-	{
-		$displaytext.="tvguide returned false with no error<br />\n";
-	}
-	if(file_exists($eitfile=$dir_video.'/'.$pathinfo['filename'].'.eit'))
+	else
+		$dom->createElement_simple('p',$td_description,array('class'=>'error'),'Error from xmltvguide: '.$guide->error);
+
+
+	if(file_exists($eitfile=$dir_video.'/'.$pathinfo['filename'].'.eit')) //Parse eit file if found
 		$programinfo['eit']=$eitparser->parse($eitfile);
 	$info_sources=array('xml','eit');
 	$info_fields=array('title','seasonepisode','description');
-	foreach($info_fields as $field)
+	foreach($info_fields as $field) //Decide which information source to use
 	{
 		foreach($info_sources as $source)
 		{
@@ -200,19 +227,21 @@ foreach ($dir as $key=>$file)
 	}
 
 	if(isset($programinfo['eit']['title']))
-		$displaytext.="EIT: {$programinfo['eit']['title']}<br />\n";
+		$p_eit=$dom->createElement_simple('p',$td_description,array('class'=>'eit'),sprintf('EIT: %s',$programinfo['eit']['title']));
 	if(isset($programinfo['eit']['raw_seasonepisodestring']))
-		$displaytext.=' '.$programinfo['eit']['raw_seasonepisodestring'];
+		$dom->createElement_simple('span',$p_eit,array('class'=>'eit'),$programinfo['eit']['raw_seasonepisodestring']);
 
-	if(isset($programinfo_final['title']) && isset($programinfo_final['seasonepisode'])) //If series title and episode num is known, find information on TVDB
+	if(is_object($tvdb) && isset($programinfo_final['title']) && isset($programinfo_final['seasonepisode'])) //If series title and episode num is known, find information on TVDB
 	{
+		$p_tvdb=$dom->createElement_simple('p',$td_description,array('class'=>'tvdb'),'TVDB: ');
+
 		$tvdb_searchstrings=array($programinfo_final['title'],preg_replace('/(.+):.+/','$1',$programinfo_final['title'])); //Search for complete title and title cut at :
 
 		foreach($tvdb_searchstrings as $search)
 		{
 			$tvdb->error='';
 
-			if(isset($tvdb_mappings) && ($tvdb_id=array_search($search,$tvdb_mappings))!==false)
+			if(isset($tvdb_mappings) && ($tvdb_id=array_search($search,$tvdb_mappings))!==false) //Check if there is a mapping between series name and tvdb id
 			{
 				$tvdb_series=$tvdb->findseries($tvdb_id);
 				break;
@@ -223,10 +252,6 @@ foreach ($dir as $key=>$file)
 				$tvdb_series=$tvdb->findseries($search,$tvdb->lang);
 				if(is_array($tvdb_series))
 					break;
-				$tvdb->error='';
-				$tvdb_series=$tvdb->findseries($search,'all'); //Try all languages
-				if(is_array($tvdb_series))
-					break;		
 			}
 		}
 
@@ -235,62 +260,51 @@ foreach ($dir as $key=>$file)
 			if($programinfo_final['seasonepisode']['season']==0)
 				$programinfo_final['seasonepisode']['season']=1;
 			$tvdbinfo=$tvdb->finnepisode($tvdb_series,$programinfo_final['seasonepisode']['season'],$programinfo_final['seasonepisode']['episode']);
-			
-			if(isset($tvdbinfo['Episode']))
-			{
-				$tvdbinfo=$tvdbinfo['Episode'];
-				$displaytext.="TVDB: {$tvdb_series['Series']['SeriesName']}<br />\n".
-				'S'.str_pad($tvdbinfo['Combined_season'],2,'0',STR_PAD_LEFT).
-				'E'.str_pad($tvdbinfo['EpisodeNumber'],2,'0',STR_PAD_LEFT);
-				if(!empty($tvdbinfo['EpisodeName']))
-					$displaytext.=' - '.$tvdbinfo['EpisodeName'];
-				$displaytext.="<br />\n";
-			}
+			if($tvdbinfo!==false)
+				$dom->createElement_simple('a',$p_tvdb,array('href'=>$tvdb->link($tvdbinfo)),$tvdb_series['Series']['SeriesName']);			
+
+			$p_tvdb->appendChild($dom->createElement('br'));
+
+			if(($episodename=$tvdb->episodename($tvdbinfo))!==false)
+				$p_tvdb->appendChild($dom->createElement('span',htmlentities($episodename)));
 		}
-
-		if(!empty($tvdb->error))
-			$displaytext.='<br />'.$tvdb->error."<br />\n";
-	}
-
-	if (file_exists($eitfile=$dir_video.$pathinfo['filename'].'.eit') && (!isset($xmlprogram) || !is_object($xmlprogram)))
-		$displaytext="---".$guide->eitparser($eitfile)."---";
-
-	if(!empty($guide->error)) //Vis feilmeldinger i tabellen
-	{
-		$displaytext.="<br />\nError: ".$guide->error;
-		$guide->error='';
-	}
-	?>
-		<tr>
-			<td><?Php echo '<a href="file://'.$winpath.$file.'">'.str_replace('  ','&nbsp;&nbsp;',htmlentities($file)).'</a>'?></td>
-			<td><?php echo $displaytext; ?></td>
-			<td>
-				<input name="navn[]" type="text" id="textfield" value="" size="6" />
-				<input name="basename[]" type="hidden" id="hiddenField" value="<?Php echo $pathinfo['filename']; ?>" />
-			</td>
-         <td>
-		 <?Php
-		 //Show snapshots
-		if(file_exists($dir_snapshots=$config['snapshotpath'].'/'.$file))
+		if(!empty($tvdb->error)) //Show TVDB errors
 		{
-			foreach(array_diff(scandir($dir_snapshots),array('.','..','Thumbs.db')) as $snapshot)
+			$p_tvdb_error=$dom->createElement_simple('span',$p_tvdb,array('class'=>'error'),'TVDB error: '.$tvdb->error);
+			unset($tvdb->error);
+		}
+	}
+
+	if(file_exists($eitfile=$dir_video.$pathinfo['filename'].'.eit') && (!isset($xmlprogram) || !is_object($xmlprogram))) //If the program is missing xml data, try to get info from eit
+		$dom->createElement_simple('span',$td_description,array('class'=>'eit'),"---".$guide->eitparser($eitfile)."---");
+
+	$td_name=$dom->createElement('td');
+	$tr->appendChild($td_name);
+	$input_name=$dom->createElement_simple('input',$td_name,array('name'=>'name[]','type'=>'text','size'=>'6'));
+	$input_basename=$dom->createElement_simple('input',$td_name,array('name'=>'basename[]','type'=>'hidden','value'=>$pathinfo['filename']));
+	
+	//Show snapshots
+	if(file_exists($dir_snapshots=$config['snapshotpath'].'/'.$file))
+	{
+		$td_snapshots=$dom->createElement_simple('td',$tr,array('class'=>'snapshots'));
+		foreach(array_diff(scandir($dir_snapshots),array('.','..','Thumbs.db')) as $snapshot)
+		{
+			$snapshotfile=$dir_snapshots.'/'.$snapshot;
+			if(is_file($snapshotfile))
 			{
-				$snapshotfile=$dir_snapshots.'/'.$snapshot;
-				if(is_file($snapshotfile))
-					echo '<img src="bilde.php?file='.$snapshotfile.'" width="15%" height="15%"/>'."\n";
+				$a_snapshot=$dom->createElement_simple('a',$td_snapshots,array('href'=>'bilde.php?file='.$snapshotfile));
+				$dom->createElement_simple('img',$a_snapshot,array('src'=>'bilde.php?file='.$snapshotfile,'height'=>'150px'));
 			}
 		}
-		?>
-        </td>
-		</tr>
-		<?Php
+	}
+	else
+		$td_name->setAttribute('colspan',2);
 
 	unset($displaytext,$xmlprogram,$programinfo,$programinfo_final);
 }
 
+$dom->createElement_simple('input',$form,array('type'=>'submit','name'=>'button','value'=>'Submit'));
+echo $dom->saveXML($form);
 ?>
-</table>
-<input type="submit" name="button" id="button" value="Submit" />
-</form>
 </body>
 </html>
