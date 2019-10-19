db5df67e459a970152254f55b50d6316b460b66c
datagutten <datagutten@datagutten.net>
2019-10-19 19:16:33 +0200

    Rewrite snapshot creator

--- createsnapshots.php
+++ createsnapshots.php
@@ -2,33 +2,64 @@
 //https://trac.ffmpeg.org/wiki/Create%20a%20thumbnail%20image%20every%20X%20seconds%20of%20the%20video
 //http://www.nrg-media.de/2010/11/creating-screenshots-with-ffmpeg-is-slow/
 
-require 'config_renamevideo.php';
-require 'tools/video.php';
-$video=new video;
-require 'xmltvtools/tvguide.class.php';
-$tvguide=new tvguide;
+require 'vendor/autoload.php';
+$config = require 'config.php';
 
-$path=$config['videopath'].$argv[1].'/';
-if(is_dir($path))
-	$dir=array_diff(scandir($config['videopath'].$argv[1]),array('.','..','Thumbs.db'));
-elseif(is_file($argv[1]))
+if(is_file($argv[1])) //Argument is an absolute path to a file
 {
-	$dir=array(basename($argv[1]));
-	$path=dirname($argv[1]).'/';
+	$pathinfo=pathinfo($argv[1]);
+	$files=array($pathinfo['basename']);
+	$dir_video=$pathinfo['dirname'];
 }
-else
-	die("Invalid: $path\n");
-foreach($dir as $file)
+elseif(is_dir($argv[1]))
+{
+	$dir_video=$argv[1];
+}
+else //Argument is a sub folder to video path
+{
+	$dir_video=$config['video_path'].$argv[1];
+	if(!file_exists($dir_video))
+		die("$dir_video does not exist\n");
+}
+
+if(!isset($files))
+	$files=array_diff(scandir($dir_video),array('.','..','Thumbs.db'));
+
+$dir_snapshots=$dir_video.'/snapshots';
+foreach($files as $file)
 {
 	$pathinfo=pathinfo($file);
-	if(!isset($pathinfo['extension']) || $pathinfo['extension']!='ts' || !$info=$tvguide->parsefilename($file))
+	try {
+        $info = datagutten\dreambox\recording_info::parse_file_name($file);
+    }
+    catch (InvalidArgumentException $e)
+    {
+        echo $e->getMessage()." $file\n";
+        continue;
+    }
+    if(file_exists($dir_snapshots.'/'.$file))
+        continue;
+
+	if(!isset($pathinfo['extension']) || $pathinfo['extension']!='ts')
 		continue;
 
 	echo "Creating snapshots for $file\n";
-
-	$steps=$video->snapshotsteps($path.$file,4,true,true,true);
-	$snapshots=$video->snapshots($path.$file,$steps,$path.'/snapshots/');
-	//die();
+    try {
+        $steps=video::snapshotsteps($dir_video.'/'.$file, 4, true, true);
+    }
+    catch (DependencyFailedException $e)
+    {
+        echo $e->getMessage();
+        break;
+    }
+    try {
+        $snapshots = video::snapshots($dir_video . '/' . $file, $steps, $dir_snapshots);
+    }
+    catch (FileNotFoundException|Exception $e)
+    {
+        echo $e->getMessage()."\n";
+        continue;
+    }
 	if(file_exists($dir_title="../snapshottitle/pictures/$file/crop/"))
 	{
 		echo "Copy title image\n";
