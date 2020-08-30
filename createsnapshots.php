a10711badf75741c2cba9198cfc0c50cfa98db5d
datagutten <datagutten@datagutten.net>
2020-08-30 19:52:12 +0200

    Use new classes in createsnapshots.php

--- createsnapshots.php
+++ createsnapshots.php
@@ -2,65 +2,65 @@
 //https://trac.ffmpeg.org/wiki/Create%20a%20thumbnail%20image%20every%20X%20seconds%20of%20the%20video
 //http://www.nrg-media.de/2010/11/creating-screenshots-with-ffmpeg-is-slow/
 
+use datagutten\renamevideo\FilePath;
+use datagutten\renamevideo\Recording;
+use datagutten\tools\files\files;
 use datagutten\video_tools\video;
 use datagutten\video_tools\exceptions as video_exceptions;
+use datagutten\xmltv\tools\exceptions\InvalidFileNameException;
+use datagutten\xmltv\tools\exceptions\XMLTVException;
+use Symfony\Component\Filesystem\Filesystem;
 
 require 'vendor/autoload.php';
 $config = require 'config.php';
+$filesystem = new Filesystem();
+umask(0);
 
-if(is_file($argv[1])) //Argument is an absolute path to a file
-{
-	$pathinfo=pathinfo($argv[1]);
-	$files=array($pathinfo['basename']);
-	$dir_video=$pathinfo['dirname'];
-}
-elseif(is_dir($argv[1]))
-{
-	$dir_video=$argv[1];
-}
-else //Argument is a sub folder to video path
-{
-	$dir_video=$config['video_path'].'/'.$argv[1];
-	if(!file_exists($dir_video))
-		die("$dir_video does not exist\n");
+if (is_file($argv[1])) { //Argument is an absolute path to a file
+    $pathinfo = pathinfo($argv[1]);
+    $files = array($pathinfo['basename']);
+    $dir_video = $pathinfo['dirname'];
+    $all = true;
+} elseif (is_dir($argv[1])) { //Argument is an absolute path to a folder
+    $dir_video = $argv[1];
+    $all = true;
+} else { //Argument is a sub folder to video path
+    try {
+        $folder = new FilePath($config['video_path'], $argv[1]);
+        $dir_video = $folder->folder;
+    } catch (FileNotFoundException $e) {
+        die($e->getMessage() . "\n");
+    }
+    $all = false;
 }
 
-if(!isset($files))
-	$files=array_diff(scandir($dir_video),array('.','..','Thumbs.db'));
+$file_tools = new datagutten\tools\files\files();
+$files = $file_tools->get_files($dir_video, ['ts'], false);
+$dir_snapshots=files::path_join($dir_video, 'snapshots');
+foreach ($files as $file) {
+    if (!$all) {
+        try {
+            datagutten\dreambox\recording_info::parse_file_name($file);
+            $recording = new Recording($file, $all);
+        } catch (InvalidFileNameException|FileNotFoundException|XMLTVException $e) {
+            continue;
+        }
+    }
 
-$dir_snapshots=$dir_video.'/snapshots';
-foreach($files as $file)
-{
-	$pathinfo=pathinfo($file);
-    if(file_exists($dir_snapshots.'/'.$file))
+    if (file_exists($recording->snapshotFolder('time')))
         continue;
 
-	if(!isset($pathinfo['extension']) || $pathinfo['extension']!='ts')
-		continue;
-
-	echo "Creating snapshots for $file\n";
+    echo "Creating snapshots for $file\n";
     try {
-        $steps=video::snapshotsteps($dir_video.'/'.$file, 4, true, true);
-    }
-    catch (DependencyFailedException|video_exceptions\DurationNotFoundException $e)
-    {
-        echo $e->getMessage();
+        $steps = video::snapshotsteps($file, 4, true, true);
+    } catch (DependencyFailedException|video_exceptions\DurationNotFoundException $e) {
+        echo $e->getMessage() . "\n";
         break;
     }
     try {
-        $snapshots = video::snapshots($dir_video . '/' . $file, $steps, $dir_snapshots);
-    }
-    catch (FileNotFoundException|Exception $e)
-    {
-        echo $e->getMessage()."\n";
+        $snapshots = video::snapshots($file, $steps, $recording->snapshotFolder('time'));
+    } catch (FileNotFoundException|Exception $e) {
+        echo $e->getMessage() . "\n";
         continue;
     }
-	if(file_exists($dir_title="../snapshottitle/pictures/$file/crop/"))
-	{
-		echo "Copy title image\n";
-		foreach(array_diff(scandir($dir_title),array('.','..','Thumbs.db')) as $key=>$picture)
-		{
-			copy($dir_title.'/'.$picture,$path.'/snapshots/'.$file.'/900'.$key.'.png');
-		}
-	}
 }
